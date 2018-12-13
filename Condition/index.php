101ad4975eb3b6019e222def03c3fe3682bf07fb
Samolberg <l.samolberg@khk.ee>
2018-12-13 16:08:05 +0200

    Tingimuslause värvid

--- Condition/index.php
+++ Condition/index.php
@@ -7,12 +7,16 @@
 // }
 
 echo '<h4>Tingimuslaused</h4>';
-
+$varv = '';
 $arv = rand(0, 100); //juhuslik täisarv vahemikus 0-100
 
-if($arv % 2 == 0){
+if($arv > 0 and $arv < 25){
     //antud arv on paaris ja on roheline
     echo '<p style="color:green">'.$arv.'</p>';
-} else {
+} else if ($arv > 25 and $arv < 50) {
     echo '<p style="color:red">'.$arv.'</p>';
-};
+} else if ($arv > 50 and $arv < 75) {
+    echo '<p style="color:orange">'.$arv.'</p>';
+} else if ($arv > 75 and $arv < 100) {
+    echo '<p style="color:black">'.$arv.'</p>';
+}
