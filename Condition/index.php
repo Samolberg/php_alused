47ca67cee2994cd1f7faa78709eb40856a1643f4
Samolberg <l.samolberg@khk.ee>
2018-12-13 16:18:41 +0200

    Tingimuslause foor

--- Condition/index.php
+++ Condition/index.php
@@ -7,18 +7,20 @@
 // }
 
 echo '<h4>Tingimuslaused</h4>';
-$varv = '';
-$arv = rand(0, 100); //juhuslik täisarv vahemikus 0-100
 
-if($arv > 0 and $arv < 25){
-    //antud arv on paaris ja on roheline
-    $varv = 'aqua';
-} else if ($arv > 25 and $arv < 50) {
-    $varv = 'red';
-} else if ($arv > 50 and $arv < 75) {
-    $varv = 'green';
-} else if ($arv > 75 and $arv < 100) {
-    $varv = 'orange';
+$foor = 'roheline';
+
+switch ($foor){
+    case 'punane':
+        echo '<p style="color:red">Punane!</p>';
+        break;
+    case 'kollane':
+        echo '<p style="color:orange">Kollane!</p>';
+        break;
+    case 'roheline':
+        echo '<p style="color:green">Roheline!</p>';
+        break;
+    default:
+        echo '<p>miongi teine!</p>';
+        break;
 }
-
-echo '<p style="color: '.$varv.'">'.$arv.'</p>';
