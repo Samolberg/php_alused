53386155a88ad4ee6f32990468b1ad7c307cc5ab
Samolberg <l.samolberg@khk.ee>
2018-12-13 15:08:55 +0200

    Väljastus php ja html

--- print/index.php
+++ print/index.php
@@ -1,3 +1,11 @@
+<!doctype html>
+<html>
+    <head>
+<title>
+</title>
+    </head>
+<body>
+<h4>TERE SEALPOOL PIKSLEID</h4>
 <?php
 // teksti väljastus print käsuga
 print 'See on esimene lause PHP keeles'.'<br>';
@@ -5,3 +13,6 @@ print 'See on teine lause PHP keeles';
 
 // teksti väljastus echo käsuga
 echo 'See tekst on echo abil väljastatud'.'<br>';
+?>
+</body>
+</html>
