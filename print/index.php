40755dfd3d4681d088033cc45fa0957b7ed3936b
Samolberg <l.samolberg@khk.ee>
2018-12-13 15:04:55 +0200

    Väljastus print ja echo

---
+++ print/index.php
@@ -0,0 +1,7 @@
+<?php
+// teksti väljastus print käsuga
+print 'See on esimene lause PHP keeles'.'<br>';
+print 'See on teine lause PHP keeles';
+
+// teksti väljastus echo käsuga
+echo 'See tekst on echo abil väljastatud'.'<br>';
