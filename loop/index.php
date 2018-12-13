28a02b8fb5491fe3c87e2bd1ee8fde1f316a4618
Samolberg <l.samolberg@khk.ee>
2018-12-13 16:46:02 +0200

    loop värvilised kastid (table)

--- loop/index.php
+++ loop/index.php
@@ -9,12 +9,16 @@
  **/
 
 echo '<h4>Tsüklid - for</h4>';
-$varv = '';
-for($arv = 1; $arv <= 10; $arv++){
-    if($arv % 2 == 0){
-        $varv = 'green';
-    } else {
-        $varv = 'red';
+echo '<table>';
+for($rida = 1; $rida <= 5; $rida++) {
+    $varv = '#';
+    for ($kord = 1; $kord <= 6; $kord++){
+        $varv = $varv.dechex(rand(0, 15));
     }
-    echo '<p style="color: '.$varv.'">'.$arv.'</p>';
+    echo '<tr>';
+        echo '<td style="background: '.$varv.'">';
+        echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp';
+        echo '</td>';
+     echo '</tr>';
 }
+echo '</table>';
