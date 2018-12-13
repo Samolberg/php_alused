31ed101e098190816b5be4d7308daa24cd91afc8
Samolberg <l.samolberg@khk.ee>
2018-12-13 15:27:54 +0200

    Defineerimine ja kasutamine

---
+++ Variables/index.php
@@ -0,0 +1,16 @@
+<?php
+// muutujate defineerimine
+// $muutujaNimi = väärtus;
+
+$taisArv = 7;
+$reaalarv = 3.14;
+$sone ='kodu';
+$toevaartusTrue = true;
+$toevaartusFalse = false;
+//False väljastab tühja kirjelduse
+
+echo 'Täisarv = '.$taisArv.'<br>';
+echo 'Reaalarv = '.$reaalarv.'<br>';
+echo 'Sõne = '.$sone.'<br>';
+echo 'TõeväärtusTrue = '.$toevaartusTrue.'<br>';
+echo 'TõeväärtusFalse = '.$toevaartusFalse.'<br>';
