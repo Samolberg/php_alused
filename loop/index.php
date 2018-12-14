df3d729c6c488bfc891520127525f3961a9950f2
Samolberg <l.samolberg@khk.ee>
2018-12-14 10:30:50 +0200

    loop värvilised kastid (tabeliks tehtud)

--- loop/index.php
+++ loop/index.php
@@ -10,15 +10,18 @@
 
 echo '<h4>Tsüklid - for</h4>';
 echo '<table>';
-for($rida = 1; $rida <= 5; $rida++) {
-    $varv = '#';
-    for ($kord = 1; $kord <= 6; $kord++){
-        $varv = $varv.dechex(rand(0, 15));
-    }
+for($rida = 1; $rida <= 5; $rida++){
     echo '<tr>';
-        echo '<td style="background: '.$varv.'">';
-        echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp';
+    for($veerg = 1; $veerg <= 5; $veerg++) {
+        $varv = '#';
+        for($kord = 1; $kord <= 6; $kord++){
+            $varv = $varv.dechex(rand(0,15));
+        }
+        echo '<td style="background: ' . $varv . '">';
+        echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
         echo '</td>';
-     echo '</tr>';
+    }
+    echo '</tr>';
 }
 echo '</table>';
+
