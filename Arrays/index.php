70c55c672d00623c19b6397d60737470d40ba7b2
Samolberg <l.samolberg@khk.ee>
2018-12-18 12:18:38 +0200

    Array merge ja array shift

--- Arrays/index.php
+++ Arrays/index.php
@@ -11,3 +11,19 @@ print_r($kasutajad);
 print_r($lisaKasutajad);
 echo '</pre>';
 //var_dump($kasutajad);
+
+//massiivi pikkus
+$kasutajadKokku = count($kasutajad);
+for($i = 0; $i < $kasutajadKokku; $i++){
+    echo $kasutajad[$i].'<br>';
+}
+
+array_shift($kasutajad);
+for($i = 0; $i < $kasutajadKokku; $i++){
+    echo $kasutajad[$i].'<br>';
+}
+
+$uuedKasutajad = array_merge($kasutajad, $lisaKasutajad);
+for($i = 0; $i < $uuedKasutajad; $i++){
+    echo $uuedKasutajad[$i].'<br>';
+}
