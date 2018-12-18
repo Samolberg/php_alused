7e8b47742673cf6134cae1e75116b1a3b7da1f7c
Samolberg <l.samolberg@khk.ee>
2018-12-18 12:39:23 +0200

    Kasutajate värvimine vastavalt massiivi elemendile mis asuv indexiga 3 'mees' 'naine'

--- Arrays/index.php
+++ Arrays/index.php
@@ -1,35 +1,34 @@
 <?php
-$kasutajad = array(); //tühi massiiv
-//andmete lisamine massiivi
-$kasutajad[] = 'alice';
-$kasutajad[] = 'bob';
-array_push($kasutajad, 'lucy');
-
-$lisaKasutajad = array('mark', 'george'); //andmetega massiiv
-echo '<pre>';
-print_r($kasutajad);
-print_r($lisaKasutajad);
-echo '</pre>';
-//var_dump($kasutajad);
-
-//massiivi pikkus
-$kasutajadKokku = count($kasutajad);
-for($i = 0; $i < $kasutajadKokku; $i++){
-    echo $kasutajad[$i].'<br>';
+$kasutajad = array(
+    array(
+        'alice',
+        'Alice',
+        'Liddell',
+        'female'
+    ),
+    array(
+        'bob',
+        'Bob',
+        'Builder',
+        'male'
+    )
+);
+/*foreach ($kasutajad as $kasutaja){
+    foreach ($kasutaja as $info){
+        echo $info.'<br>';
+    }
+    echo '<hr>';
 }
+*/
 
-array_shift($kasutajad);
-for($i = 0; $i < $kasutajadKokku; $i++){
-    echo $kasutajad[$i].'<br>';
-}
-
-$uuedKasutajad = array_merge($kasutajad, $lisaKasutajad);
-for($i = 0; $i < count($uuedKasutajad); $i++){
-    echo $uuedKasutajad[$i].'<br>';
-}
-
-echo '<hr>';
-
-foreach ($uuedKasutajad as $kasutaja){
-    echo $kasutaja.'<br>';
+for ($i = 0; $i < count($kasutajad); $i++){
+    foreach ($kasutajad[$i] as $info){
+        if($kasutajad[$i][3] == 'female'){
+        echo '<div style="color:red">';
+    } else {
+        echo '<div style="color:blue">';
+    }
+    echo $info.'</div>';
+    }
+    echo '<hr>';
 }
