f5ba474a1ba04dc0e1e2e36050cce90a032cca71
l.samolberg <l.samolberg@khk.ee>
2019-01-22 12:25:39 +0200

    tellimine.php fail lisatud 'tk'

--- form/tellimine.php
+++ form/tellimine.php
@@ -5,5 +5,14 @@
  * Date: 22.01.2019
  * Time: 11:02
  */
-echo'<h1>Tellimine</h1>';
+echo '<h1>Tellimine</h1>';
+
+//lisab vormist saadud andmed muutujasse
+$toode1 = $_GET['t1'];
+$toode2 = $_GET['t2'];
+$toode3 = $_GET['t3'];
+
+echo 'Toode 1: ' . $toode1 . 'tk<br>';
+echo 'Toode 2: ' . $toode2 . 'tk<br>';
+echo 'Toode 3: ' . $toode3 . 'tk<br>';
 ?>
