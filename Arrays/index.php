ae6bb0a7f29e241363f793345d2a7bc2c93511af
Samolberg <larssamolberg@gmail.com>
2019-01-27 12:50:39 +0200

    tabel

--- Arrays/index.php
+++ Arrays/index.php
@@ -1,4 +1,12 @@
 <?php
+$tabel = array(
+    array(
+        'Username',
+        'First name',
+        'Last name',
+        'Gender'
+    )
+);
 $kasutajad = array(
     array(
         'alice',
@@ -21,14 +29,33 @@ $kasutajad = array(
 }
 */
 
-for ($i = 0; $i < count($kasutajad); $i++){
-    foreach ($kasutajad[$i] as $info){
-        if($kasutajad[$i][3] == 'female'){
-        echo '<div style="color:red">';
-    } else {
-        echo '<div style="color:blue">';
-    }
-    echo $info.'</div>';
+for ($i = 0; $i < count($kasutajad); $i++) {
+    foreach ($kasutajad[$i] as $info) {
+        if ($kasutajad[$i][3] == 'female') {
+            echo '<div style="color:red">';
+        } else {
+            echo '<div style="color:blue">';
+        }
+        echo $info . '</div>';
     }
     echo '<hr>';
+
+    echo '<table style="border-collapse:collapse">';
+    for ($i = 0; $i < count($kasutajad); $i++) {
+        echo '<tr style="width: 20px; text-align: center; border:solid 1px black;">';
+        foreach ($tabel[$i] as $table) {
+            echo '<th style="width: 20px; text-align: center; border:solid 1px black;">' . $table;
+            echo '</th>';
+        }
+        echo '</tr>';
+        foreach ($kasutajad[$i] as $info) {
+            if ($kasutajad[$i][3] == 'female') {
+                echo '<td style="width: 20px; text-align: center; border:solid 1px black;background:red; color:white">';
+            } else {
+                echo '<td style="width: 20px; text-align: center; border:solid 1px black;background:blue; color:white">';
+            }
+            echo $info . '</td>';
+        }
+    }
+    echo '</table>';
 }
