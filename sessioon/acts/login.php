84f6d253aab5c0d4a98f4fbcdac8efabdeaf3bc2
l.samolberg <l.samolberg@khk.ee>
2019-01-28 15:26:25 +0200

    loodud kasutaja sessioon

--- sessioon/acts/login.php
+++ sessioon/acts/login.php
@@ -17,4 +17,11 @@ $ikt = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
 $sql = 'SELECT * FROM user WHERE username="'.$username.'" AND password="'.md5($password).'"';
 $users = getData($sql, $ikt);
 // väljastame andmed
-echo $users[0]['first_name'].' '.$users[0]['last_name'];
+//echo $users[0]['first_name'].' '.$users[0]['last_name'];
+
+// loome kasutaja sessiioon, kui ta andmebaasis olemas
+if($users !== false){
+    session_start();
+    $_SESSION['user'] = $users[0];
+    header('Location: ../index.php');
+}
--- sessioon/index.php
+++ sessioon/index.php
@@ -5,6 +5,13 @@
  * Date: 28.01.2019
  * Time: 15:08
  */
+session_start();
 require_once 'fnk.php';
-$loginForm = htmlFromFile('login');
-echo $loginForm;
+//$loginForm = htmlFromFile('login');
+//echo $loginForm;
+if(isset($_SESSION['user'])){
+    echo 'Tere tulemast, '.$_SESSION['user']['last_name'].'!<br>';
+} else {
+    $loginForm = htmlFromFile('login');
+    echo $loginForm;
+}
