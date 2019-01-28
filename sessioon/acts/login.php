ae8d27cdb188570c21fd77a09fea860771642df8
l.samolberg <l.samolberg@khk.ee>
2019-01-28 15:20:50 +0200

    sisestatud andmete kontroll andmebaasiga

--- sessioon/acts/login.php
+++ sessioon/acts/login.php
@@ -5,7 +5,16 @@
  * Date: 28.01.2019
  * Time: 15:15
  */
+
+require_once '../db/conf.php';
+require_once '../db/db_fnk.php';
 // andmed HTML vormi poolt
 $username = $_GET['uname'];
 $password = $_GET['upass'];
-echo $username.' '.$password;
+
+$ikt = connect_db(DBHOST, DBUSER, DBPASS, DBNAME);
+// küsime kasutaja andmete olemasolu andmebaasis
+$sql = 'SELECT * FROM user WHERE username="'.$username.'" AND password="'.md5($password).'"';
+$users = getData($sql, $ikt);
+// väljastame andmed
+echo $users[0]['first_name'].' '.$users[0]['last_name'];
