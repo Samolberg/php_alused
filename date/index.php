353e6ddcc7c38e5b4eb6b7dc4ff8594a26572a0f
l.samolberg <l.samolberg@khk.ee>
2019-01-28 13:37:25 +0200

    8.6 ja 8.7 date ülesanne maailmalõpp ja juubel

--- date/index.php
+++ date/index.php
@@ -81,13 +81,28 @@ $jaan = mktime(0, 0, 0, 6, 24, 2019);
 echo 'Jaanipäevani on ' . round(date($jaan - strtotime('now')) / 3600 / 24) . ' päeva.';
 echo '<hr>';
 
-$päev1 = date('Y', mktime(0,0,0,11,6,1980));
-$päev2 = date('Y', mktime(0,0,0,2,20,1993));
+$päev1 = date('Y', mktime(0, 0, 0, 11, 6, 1980));
+$päev2 = date('Y', mktime(0, 0, 0, 2, 20, 1993));
 if ($synd1 > $synd2) {
-    echo 'Noorem '.($päev1-$päev2).' aastat';
+    echo 'Noorem ' . ($päev1 - $päev2) . ' aastat';
+} else {
+    echo 'Vanem ' . ($päev2 - $päev1) . ' aastat';
 }
-else {
-    echo 'Vanem '.($päev2-$päev1).' aastat';
+
+echo '<hr>';
+
+$lopp = date('d.m.Y', mktime(0, 0, 0, 2, 29, 2016));
+echo 'Maailmalõpp saabub kuupäeval ' . $lopp;
+
+echo '<hr>';
+
+$synd = date('Y', mktime(0, 0, 0, 7, 18, 1993));
+$jarg = date('Y');
+echo $jarg + 1 - $synd . '<br>';
+if (($jarg + 1 - $synd) % 5 == 0) {
+    echo 'Juubel on tulekul';
+} else {
+    echo 'Juubelini on veel aega';
 }
 echo '<hr>';
 ?>
