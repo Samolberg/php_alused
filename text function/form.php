88ec4b5ca481f8d99ec2fbdcf68dc5285d3c8521
l.samolberg <l.samolberg@khk.ee>
2019-01-28 13:57:15 +0200

    9.3 teksti asendus

--- text function/form.php
+++ text function/form.php
@@ -9,10 +9,16 @@
 //$nimi = $_GET['name'];
 //echo 'Tere, '.ucfirst(strtolower($nimi));
 
-echo '<hr>';
-echo '<h3>9.2</h3>';
-$nimi = $_GET['name'];
-$upper = strtoupper($nimi);
-$dotter = implode('.',str_split($upper));
-echo $dotter;
-echo '<hr>';
+//echo '<hr>';
+//echo '<h3>9.2</h3>';
+//$nimi = $_GET['name'];
+//$upper = strtoupper($nimi);
+//$dotter = implode('.',str_split($upper));
+//echo $dotter;
+//echo '<hr>';
+
+echo '<h3>9.3</h3>';
+$tekst = $_GET['tekst'];
+$otsi = array('noob', 'pleb', 'feeder', 'toxic');
+$asenda = '****';
+echo str_replace($otsi, $asenda, $tekst);
