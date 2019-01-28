673fb729d48df8f01e5623d75c333caf286e7394
l.samolberg <l.samolberg@khk.ee>
2019-01-28 15:57:24 +0200

    sidestatud tüklis olev massiiv

--- Arrays/index.php
+++ Arrays/index.php
@@ -1,9 +1,15 @@
-
+<html>
+<head>
+    <title>Listing 7.2</title>
+</head>
+<body>
 <?php
-$users = array('Bert', 'Sharon', 'Betty', 'Harry');
-foreach ($users as $val) {
-    print $val.'<br>';
+$character = array('name'=> 'Bob', 'occupation'=> 'superhero', 'age'=>30, 'special power'=> 'x-ray vision');
+foreach ($character as $key=>$val) {
+    print $key = $val.'<br>';
 }
 ?>
 
+</body>
 
+</html>
