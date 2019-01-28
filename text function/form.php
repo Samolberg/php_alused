e940294a1f72f4cf9e3b2b2cb87683dba35c7228
l.samolberg <l.samolberg@khk.ee>
2019-01-28 13:48:33 +0200

    9.2 tekst implode

--- text function/form.php
+++ text function/form.php
@@ -5,6 +5,14 @@
  * Date: 28.01.2019
  * Time: 12:21
  */
-echo '<h3>9.1</h3>';
+//echo '<h3>9.1</h3>';
+//$nimi = $_GET['name'];
+//echo 'Tere, '.ucfirst(strtolower($nimi));
+
+echo '<hr>';
+echo '<h3>9.2</h3>';
 $nimi = $_GET['name'];
-echo 'Tere, '.ucfirst(strtolower($nimi));
+$upper = strtoupper($nimi);
+$dotter = implode('.',str_split($upper));
+echo $dotter;
+echo '<hr>';
